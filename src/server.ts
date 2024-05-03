ea44c10f85c2733f66a9f5ad4fadb1cf17396ab7
DESKTOP-7EOCORJ\admin <huytq@ics-p.vn>
2024-05-03 15:55:09 +0700

    Add validate environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import { IndexRoute } from './modules/index';
 import App from "./app";
 import 'dotenv/config';
+import validateEnv from './core/utils/validate_env';
 
+validateEnv();
 const routes = [new IndexRoute()];
 const app = new App(routes);
 
