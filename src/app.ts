f0fd450c3d50c273e912459d63a2920d6b7d9b19
DESKTOP-7EOCORJ\admin <huytq@ics-p.vn>
2024-05-03 15:49:15 +0700

    add middleware

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,56 @@
 import { Route } from 'core/interfaces';
+import { Logger } from './core/utils';
+import cors from 'cors';
 import express from 'express';
+import helmet from 'helmet';
+import hpp from 'hpp';
 import mongoose from 'mongoose';
+import morgan from 'morgan';
 
 class App {
     public app: express.Application;
     public port: string | number;
+    public production: boolean;
+
     constructor(routes: Route[]) {
         this.app = express();
         this.port = process.env.PORT || 5000;
+        this.production = process.env.NODE_ENV == "production" ? true : false;
 
         this.initializeRoutes(routes);
         this.connectToDatabase();
+        this.initializeMiddleware();
     }
 
     public listen() {
         this.app.listen(this.port, () => {
-            console.log(`Server is running on port ${this.port}`);
+            Logger.info(`Server is running on port ${this.port}`);
         })
     }
 
     private connectToDatabase() {
         try {
             const connectionString = process.env.MONGODB_URI;
-            if(!connectionString){
-                console.log('ConnectionString is invalid');
+            if (!connectionString) {
+                Logger.error('ConnectionString is invalid');
                 return;
             }
             mongoose.connect(connectionString);
-            console.log('Database connected...');
+            Logger.info('Database connected...');
         } catch (error) {
-            console.log('Connect to database error.');
+            Logger.error('Connect to database error.');
+        }
+    }
+
+    private initializeMiddleware() {
+        if (this.production) {
+            this.app.use(hpp());
+            this.app.use(helmet());
+            this.app.use(morgan('combined'));
+            this.app.use(cors({ origin: 'your.domain.com', credentials: true }));
+        } else {
+            this.app.use(morgan('dev'));
+            this.app.use(cors({ origin: true, credentials: true }));
         }
     }
 
