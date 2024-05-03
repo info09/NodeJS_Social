b83027812653747d1701f6cf86c1060a4c0a2fcf
DESKTOP-7EOCORJ\admin <huytq@ics-p.vn>
2024-05-03 15:19:54 +0700

    connect database

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { Route } from 'core/interfaces';
 import express from 'express';
+import mongoose from 'mongoose';
 
 class App {
     public app: express.Application;
@@ -9,15 +10,26 @@ class App {
         this.port = process.env.PORT || 5000;
 
         this.initializeRoutes(routes);
+        this.connectToDatabase();
     }
 
-    public listen(){
+    public listen() {
         this.app.listen(this.port, () => {
             console.log(`Server is running on port ${this.port}`);
         })
     }
 
-    private initializeRoutes(routes: Route[]){
+    private connectToDatabase() {
+        try {
+            const connectionString = 'mongodb+srv://huytq3103:RK199tLWX7lfjqEU@cluster0.3ozq9hc.mongodb.net/social_network?retryWrites=true&w=majority&appName=Cluster0';
+            mongoose.connect(connectionString);
+            console.log('Database connected...');
+        } catch (error) {
+            console.log('Connect to database error.');
+        }
+    }
+
+    private initializeRoutes(routes: Route[]) {
         routes.forEach((route) => {
             this.app.use('/', route.router);
         })
