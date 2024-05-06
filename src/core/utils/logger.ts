0e5a1b96eed01e637b7db5cf26e615c064243565
DESKTOP-7EOCORJ\admin <huytq@ics-p.vn>
2024-05-06 10:24:02 +0700

    format logger

--- src/core/utils/logger.ts
+++ src/core/utils/logger.ts
@@ -1,11 +1,21 @@
 import winston from "winston";
 
 const Logger: winston.Logger = winston.createLogger({
-    transports: [new winston.transports.Console()],
+    //transports: [new winston.transports.Console()],
+    transports: [new winston.transports.File({
+        filename: 'logs/error.log',
+        level: 'error'
+    })],
     format: winston.format.combine(
-        winston.format.colorize({all: true}),
+        winston.format.colorize({ all: true }),
         winston.format.simple()
     )
 })
+
+if (process.env.NODE_ENV == "development") {
+    Logger.add(new winston.transports.Console({
+        format: winston.format.simple()
+    }))
+}
 
 export default Logger;
